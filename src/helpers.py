395b3b40ff7dbec9da0e6ee586b7cb706800c714
pgreis <paulgreis@gmx.de>
2025-09-27 13:22:40 +0200

    :tada: Initial commit

---
+++ src/helpers.py
@@ -0,0 +1,10 @@
+import json
+import yaml
+
+def read_json(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+def read_yaml(file_path):
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
